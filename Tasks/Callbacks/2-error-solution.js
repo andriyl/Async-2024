69bbf8abe57b412bf3086636dea64f94f9c50c22
Andriy Leliv <andrii.leliv@gmail.com>
2024-03-11 13:56:52 -0400

    2-error-solution

---
+++ Tasks/Callbacks/2-error-solution.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const total = (items, callback) => {
+  let result = 0;
+  for (const item of items) {
+    if (item.price < 0) {
+      const errorMessage = `${item.name} has negative price: ${item.price}`;
+      return void callback(new Error(errorMessage));
+    }
+    result += item.price;
+  }
+  callback(null, result);
+};
+
+const electronics = [
+  { name: 'Laptop', price: -1500 },
+  { name: 'Keyboard', price: 100 },
+  { name: 'HDMI cable', price: 10 },
+];
+
+total(electronics, (error, money) => {
+  if (error) return console.error({ error });
+  console.log({ money });
+});
