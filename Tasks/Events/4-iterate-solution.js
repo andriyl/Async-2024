bee25668497fc4133228bceda902222bce756b43
Andriy Leliv <andrii.leliv@gmail.com>
2024-03-20 12:45:15 -0400

    Events/4-iterate-solution

---
+++ Tasks/Events/4-iterate-solution.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const { EventEmitter, on } = require('node:events');
+const purchase = new EventEmitter({ captureRejections: true });
+
+const electronics = [
+  { name: 'Laptop', price: 1500 },
+  { name: 'Keyboard', price: 100 },
+  { name: 'HDMI cable', price: 10 },
+];
+
+(async () => {
+  const iterator = on(purchase, 'add');
+  for await (const [value] of iterator) {
+    console.log(value);
+  }
+})();
+
+for (const item of electronics) {
+  purchase.emit('add', item);
+}
